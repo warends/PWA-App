7c2b637f6beeb029d7fe0246f202ac611119be12
Will Arends <Will.Arends@indigoslate.com>
2017-12-08 09:14:20 -0800

    init pwa

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', function(event){
+    console.log('installed ' + event);
+});
+
+self.addEventListener('activate', function(event){
+    console.log('activated ' + event);
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+    console.log('fetched '+ event);
+    event.respondWith(fetch(event.request));
+});
